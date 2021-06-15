8db8a5bf6bd847494ffa69e47839df411fabd8e3
victor vargas <vict.vargass@duocuc.cl>
2021-06-15 16:22:28 -0400

    widgets agregado

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,20 @@
 from django import forms
-from django import forms
-from django.db.models.fields import EmailField
-from django.forms import ModelForm
+from django.db.models.fields import CharField, EmailField
+from django.forms import ModelForm, widgets
 from .models import usuarios
 
-
-class registroUsuario (ModelForm):
+class registroUsuario (forms.ModelForm):
 
     class Meta:
         model = usuarios
-        fields=['nombreUsuario','emailUsuario','contraseñaUsuario']
-
+        fields=['nombreUsuario','emailUsuario', 'contraseñaUsuario']
+        label={
+            'nombreUsuario': 'Nombre',
+            'emailUsuario': 'Correo electronico',
+            'contraseñaUsuario': 'Contraseña'
+        }
+        widgets={
+            'nombreUsuario': forms.TextInput(attrs={'class':'form-control','placeholder':'Ingrese su nombre'}),
+            'emailUsuario': forms.TextInput(attrs={'class':'form-control','placeholder':'Ingrese su correo'}),
+            'contraseñaUsuario': forms.PasswordInput(attrs={'class':'form-control','placeholder':'Ingrese su contraseña'})
+        }
