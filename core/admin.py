d5b1824f919cd7d9102ddfbd4e29c56f3025658b
victor vargas <vict.vargass@duocuc.cl>
2021-06-14 20:45:42 -0400

    modelos registrados en admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import usuarios, solicitudesContacto,solicitudObra,obrasPublicadas
 
 # Register your models here.
+admin.site.register(usuarios)
+admin.site.register(solicitudObra)
+admin.site.register(solicitudesContacto)
+admin.site.register(obrasPublicadas)
