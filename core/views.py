3a43dfa9771e5325216f8a70e7175446f100cbf8
victor vargas <vict.vargass@duocuc.cl>
2021-06-14 20:51:29 -0400

    views actualizado

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import registroUsuario
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,11 @@ def home(request):
 
 def formulario(request):
     return render(request, 'core/formulario.html')
+
+def registroUsuarios(request):
+    datos={'form': registroUsuario()}
+    if request.method == 'POST':
+        registo_usuario = registroUsuario(request.POST)
+        if registo_usuario.is_valid:
+            registo_usuario.save()
+    return render(request, 'core/registro.html', datos)
