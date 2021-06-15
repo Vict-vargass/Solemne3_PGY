3796b63f8dffa5d5156689a4d78ba0f4ef52f409
victor vargas <vict.vargass@duocuc.cl>
2021-06-14 20:47:09 -0400

    urls actualizado

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, formulario
+from .views import home, formulario, registroUsuarios
 
 urlpatterns = [
     path('', home, name="inicio"),
     path('formulario-contacto/', formulario, name='contacto'),
+    path('registro/', registroUsuarios, name='registro')
 ]
