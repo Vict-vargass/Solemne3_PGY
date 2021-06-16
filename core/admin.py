881801c19f0ab99346da264831ad6ab672ebad3f
victor vargas <vict.vargass@duocuc.cl>
2021-06-16 19:49:25 -0400

    modelo categoria registrado

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import usuarios, solicitudesContacto,solicitudObra,obrasPublicadas
+from .models import usuarios, solicitudesContacto,solicitudObra,obrasPublicadas, CategoriaObras
 
 # Register your models here.
 admin.site.register(usuarios)
 admin.site.register(solicitudObra)
 admin.site.register(solicitudesContacto)
 admin.site.register(obrasPublicadas)
+admin.site.register(CategoriaObras)
