9d8087a206c9e7af34a2db18a83009e8c0e265b8
victor vargas <vict.vargass@duocuc.cl>
2021-06-16 19:53:54 -0400

    urls solicitud y agregar creadas

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, formulario, registroUsuarios
+from .views import home, formulario, registroUsuarios, solicitudes,add_obra
 
 urlpatterns = [
     path('', home, name="inicio"),
     path('formulario-contacto/', formulario, name='contacto'),
-    path('registro/', registroUsuarios, name='registro')
+    path('registro/', registroUsuarios, name='registro'),
+    path('solicitudes/', solicitudes, name='solicitud'),
+    path('agregar-obra/', add_obra, name='agregar'),
 ]
