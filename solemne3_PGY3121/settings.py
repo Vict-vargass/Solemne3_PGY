b8674d757c6199707c14289cb4961f41fb4b4036
victor vargas <vict.vargass@duocuc.cl>
2021-06-26 21:44:21 -0400

    media url, root y static dir agregados, static url cambiado

--- solemne3_PGY3121/settings.py
+++ solemne3_PGY3121/settings.py
@@ -118,9 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'core/static/core/'
+STATIC_URL = '/static/'
+
+STATIFILE_DIRS = (
+    '/core/static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# MEDIA
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL= 'media/'
