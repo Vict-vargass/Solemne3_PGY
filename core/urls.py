22bf6f49f0f77c175c090fe703851d8cc1d201b2
victor vargas <vict.vargass@duocuc.cl>
2021-06-26 21:56:34 -0400

    urls ajustadas, una comentada y nuevas agregadas

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import home, formulario, registroUsuarios, solicitudes,add_obra, edit_obra
-
+from .views import home, formulario, listadoObra,add_obra, edit_obra, borrar_obra,galeria #,detalleObra
+#Registro removido
 urlpatterns = [
-    path('', home, name="inicio"),
-    path('formulario-contacto/', formulario, name='contacto'),
-    path('registro/', registroUsuarios, name='registro'),
-    path('solicitudes/', solicitudes, name='solicitud'),
-    path('agregar-obra/', add_obra, name='agregar'),
-    path('editar-obra/<pk>', edit_obra, name='editar_obra')
+    path('', home, name="inicio_home"),
+    path('formulario-contacto', formulario, name='contacto'),
+    path('listado-obras', listadoObra, name='listado'),
+    path('agregar-obra', add_obra, name='agregar'),
+    path('editar-obra/<pk>', edit_obra, name='editar_obra'),
+    path('eliminar-obra/<pk>', borrar_obra, name='borrar_obra'),
+    path('galeria', galeria, name='galeria'),
+    #path('detalle-obra/<pk>', detalleObra, name='detalle_obra')
 ]
