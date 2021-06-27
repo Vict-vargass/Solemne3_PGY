56cfb20f7198fd36f5194db502d48000c31c7527
victor vargas <vict.vargass@duocuc.cl>
2021-06-26 21:50:06 -0400

    vista galeria nueva, vistas ajustadas y comentadas

--- core/views.py
+++ core/views.py
@@ -1,44 +1,58 @@
-from core.models import solicitudObra
+from core.models import Obra
 from django.shortcuts import redirect, render
-from .forms import registroUsuario, publicarObra, EditarObra
-from .models import solicitudObra
+from .forms import  publicarObra, EditarObra
+from .models import Obra
 
 # Create your views here.
 def home(request):
     return render(request,'core/home.html')
 
+def galeria(request):
+    obra = Obra.objects.all()
+    datos={
+        'obra': obra
+    }
+    return render(request,'core/galeria.html',datos)
+
+#def detalleObra(request, pk):
+#    obra = Obra.objects.get(idObra = pk)
+#    datos = {
+#        'obra': obra
+#    }
+#    return render(request,'detalleObra.html', datos)
+
 def formulario(request):
     return render(request, 'core/formulario.html')
+# VISTA QUE SERA USADA EN EL PROYECTO SEMESTRAL
+#def registroUsuarios(request):
+#    datos={'form': registroUsuario()}
+#    if request.method == 'POST':
+#        registo_usuario = registroUsuario(request.POST)
+#        if registo_usuario.is_valid:
+#            registo_usuario.save()
+#    return render(request, 'core/registro.html', datos)
 
-def registroUsuarios(request):
-    datos={'form': registroUsuario()}
-    if request.method == 'POST':
-        registo_usuario = registroUsuario(request.POST)
-        if registo_usuario.is_valid:
-            registo_usuario.save()
-    return render(request, 'core/registro.html', datos)
-
-def solicitudes(request):
-    solicitud_obra = solicitudObra.objects.all()
+def listadoObra(request):
+    listado = Obra.objects.all()
     datos = {
-        'solicitud': solicitud_obra
+        'listado': listado
     }
-    return render(request, 'core/solicitudesObras.html', datos)
+    return render(request, 'core/listadoObras.html', datos)
 
 def add_obra(request):
     datos = {
         'form': publicarObra() 
         }
     if request.method == 'POST':
-        formulario_add = publicarObra(request.POST)
+        formulario_add = publicarObra(request.POST, request.FILES)
         if formulario_add.is_valid:
             formulario_add.save()
-
-    return render(request, 'core/crearSolicitudObra.html', datos)
+            return redirect(to="inicio_home")
+    return render(request, 'core/crearObra.html', datos)
 
 
 def edit_obra(request, pk):
-    obra = solicitudObra.objects.get(idSolicitudObra = pk)
+    obra = Obra.objects.get(idObra = pk)
     datos = {
         'form': EditarObra(instance= obra) 
         }
@@ -46,5 +60,10 @@ def edit_obra(request, pk):
         formulario_edit = EditarObra(data=request.POST, instance= obra)
         if formulario_edit.is_valid:
             formulario_edit.save()
-    return render(request, 'core/editarSolicitud.html', datos)
+    return render(request, 'core/editarObra.html', datos)
 
+def borrar_obra(request , pk):
+    obra= Obra.objects.get(idObra = pk)
+    obra.delete()
+    return redirect(to="listado")
+    
